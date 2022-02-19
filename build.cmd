ee7a99a8b774879e353218e39989bfed06cf0536
brahma <brahma@noreply.codeberg.org>
2022-02-20 00:36:49 +0100

    Update 'build.cmd'

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 mkdir bapp
 copy bapp.rb bapp/bapp.rb
 echo @echo off > bapp/bapp.bat
-echo set lp=^%~dp0bapp.rb >> bapp/bapp.bat
+echo set lp=%%~dp0\bapp.rb >> bapp/bapp.bat
 type bapp.bat >> bapp/bapp.bat
